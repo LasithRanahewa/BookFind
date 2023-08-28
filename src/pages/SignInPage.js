68945065058ff4b9af6bfcb180e8065138233906
Lasith Ranahewa <lasithmrana@gmail.com>
2023-08-29 00:13:42 +0530

    add minor changes to the signin and signup pages

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,59 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { TextField, Button, FormControl, Typography } from '@material-ui/core';
+import { TextField, Button, FormControl, Typography, Link, Checkbox, FormControlLabel } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-  form: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100vh',
-  },
-  textField: {
-    margin: theme.spacing(1),
-    width: '25ch',
-  },
-  button: {
-    margin: theme.spacing(1),
-    width: '25ch',
-  },
+    form: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+    },
+    textField: {
+        margin: theme.spacing(1),
+        width: '40ch',
+    },
+    button: {
+        margin: theme.spacing(1),
+        width: '40ch',
+    },
 }));
 
 const SignInPage = () => {
-  const classes = useStyles();
+    const classes = useStyles();
+    const [rememberMe, setRememberMe] = useState(false);
 
-  return (
-    <form className={classes.form}>
-      <FormControl>
-        <Typography variant="h5" gutterBottom style={{ textAlign: 'center' }}>
-          Sign In
-        </Typography>
-        <TextField
-          className={classes.textField}
-          label="Email"
-          variant="outlined"
-          type="email"
-          required
-        />
-        <TextField
-          className={classes.textField}
-          label="Password"
-          variant="outlined"
-          type="password"
-          required
-        />
-        <Button
-          className={classes.button}
-          variant="contained"
-          color="primary"
-          type="submit"
-        >
-          Sign In
-        </Button>
-      </FormControl>
-    </form>
-  );
+    const handleRememberMeChange = (event) => {
+        setRememberMe(event.target.checked);
+    };
+
+    return (
+        <form className={classes.form}>
+            <FormControl style={{ display: 'flex', flexDirection: 'column' }}>
+                <Typography variant="h5" gutterBottom style={{ textAlign: 'center' }}>
+                    Sign In
+                </Typography>
+                <TextField
+                    className={classes.textField}
+                    label="Email"
+                    variant="outlined"
+                    type="email"
+                    required
+                />
+                <TextField
+                    className={classes.textField}
+                    label="Password"
+                    variant="outlined"
+                    type="password"
+                    required
+                />
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            checked={rememberMe}
+                            onChange={handleRememberMeChange}
+                            name="rememberMe"
+                            color="primary"
+                        />
+                    }
+                    label="Remember Me"
+                    style={{ marginLeft: '0' }}
+                />
+                <Button
+                    className={classes.button}
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                >
+                    Sign In
+                </Button>
+                <Typography variant="body1" gutterBottom style={{ textAlign: 'center' }}>
+                    No account? <Link href="/sign-up">Sign up here</Link>
+                </Typography>
+            </FormControl>
+        </form>
+    );
 };
 
 export default SignInPage;
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -3,71 +3,79 @@ import { makeStyles } from '@material-ui/core/styles';
 import { TextField, Button, FormControl, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-  form: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100vh',
-  },
-  textField: {
-    margin: theme.spacing(1),
-    width: '25ch',
-  },
-  button: {
-    margin: theme.spacing(1),
-    width: '25ch',
-  },
+    form: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+    },
+    textField: {
+        margin: theme.spacing(1),
+        width: '40ch',
+    },
+    button: {
+        margin: theme.spacing(1),
+        width: '40ch',
+    },
 }));
 
 const SignUpPage = () => {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <form className={classes.form}>
-      <FormControl>
-        <Typography variant="h5" gutterBottom style={{ textAlign: 'center' }}>
-          Sign Up
-        </Typography>
-        <TextField
-          className={classes.textField}
-          label="Name"
-          variant="outlined"
-          type="text"
-          required
-        />
-        <TextField
-          className={classes.textField}
-          label="Email"
-          variant="outlined"
-          type="email"
-          required
-        />
-        <TextField
-          className={classes.textField}
-          label="Password"
-          variant="outlined"
-          type="password"
-          required
-        />
-        <TextField
-          className={classes.textField}
-          label="Confirm Password"
-          variant="outlined"
-          type="password"
-          required
-        />
-        <Button
-          className={classes.button}
-          variant="contained"
-          color="primary"
-          type="submit"
-        >
-          Sign Up
-        </Button>
-      </FormControl>
-    </form>
-  );
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Add your submit logic here
+    };
+
+    return (
+        <form className={classes.form} onSubmit={handleSubmit}>
+            <FormControl>
+                <Typography variant="h5" gutterBottom style={{ textAlign: 'center' }}>
+                    Sign Up
+                </Typography>
+                <TextField
+                    className={classes.textField}
+                    label="Name"
+                    variant="outlined"
+                    type="text"
+                    required
+                />
+                <TextField
+                    className={classes.textField}
+                    label="Email"
+                    variant="outlined"
+                    type="email"
+                    required
+                />
+                <TextField
+                    className={classes.textField}
+                    label="Password"
+                    variant="outlined"
+                    type="password"
+                    required
+                />
+                <TextField
+                    className={classes.textField}
+                    label="Confirm Password"
+                    variant="outlined"
+                    type="password"
+                    required
+                />
+                <Button
+                    className={classes.button}
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                >
+                    Sign Up
+                </Button>
+                <Typography variant="body1" gutterBottom style={{ textAlign: 'center' }}>
+                    Already have an account? <a href="/sign-in">Sign in here</a>
+                </Typography>
+            </FormControl>
+        </form>
+    );
 };
 
 export default SignUpPage;
