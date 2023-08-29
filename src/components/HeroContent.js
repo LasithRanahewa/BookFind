019731250e09248e998acdf496052027e6488a1e
Lasith Ranahewa <lasithmrana@gmail.com>
2023-08-29 19:16:43 +0530

    modify hero page

--- src/components/HeroContent.js
+++ src/components/HeroContent.js
@@ -1,20 +1,54 @@
 import React from "react";
 import { Grid, TextField, Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Hero_Img from "../assets/heropage.png";
+
+const useStyles = makeStyles((theme) => ({
+  hero: {
+    backgroundColor: "#142850",
+    display: "flex",
+    flexDirection: "column",
+    height: "100vh",
+  },
+  herotext: {
+    marginTop: "10%",
+    color: "#DAE1E7",
+  },
+  textField: {
+    margin: theme.spacing(1),
+    width: '40ch',
+    '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+            borderColor: '#00909E',
+        },
+        '&:hover fieldset': {
+            borderColor: '#00909E',
+        },
+        '&.Mui-focused fieldset': {
+            borderColor: '#00909E',
+        },
+    },
+    '& .MuiFormLabel-root': {
+        color: '#DAE1E7',
+    },
+    '& .MuiInputBase-input': {
+        color: '#DAE1E7',
+    },
+},
+}));
 
 const HeroContent = () => {
+  const classes = useStyles();
   return (
-    <div style={{ position: "relative", top: 0, left: 0, height: "100vh" }}>
-      <Grid container spacing={2}>
+    <div className={classes.hero}>
+      <Grid container spacing={2} >
         <Grid item xs={12} md={8}>
-          <div className="hero">
+          <div className={classes.herotext}>
             <h2>e-Books Galore | Explore | Discover</h2>
-            <h1 className="herotext2">
-              IT'S NOT JUST A <br />
-              BOOKSTORE.
-            </h1>
+            <h1 >IT'S NOT JUST A <br />BOOKSTORE.</h1>
             <h2>It's a universe.</h2>
             <Box sx={{ display: "flex", alignItems: "center", width: "100%" }}>
-              <TextField
+              <TextField className={classes.textField}
                 label="Find a book"
                 variant="outlined"
                 size="small"
@@ -23,8 +57,8 @@ const HeroContent = () => {
             </Box>
           </div>
         </Grid>
-        <Grid item xs={12} md={4}>
-          <p>img</p>
+        <Grid item xs={12} md={4} >
+          <img src={Hero_Img} alt="hero" />
         </Grid>
       </Grid>
     </div>
