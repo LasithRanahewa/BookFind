47f05101e54a88e6e00f09df6616fa15bc79e01a
Lasith Ranahewa <lasithmrana@gmail.com>
2023-08-29 12:48:43 +0530

    minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,66 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(0),
   },
   title: {
     flexGrow: 1,
   },
 }));
 
+const sty = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#142850',
+    },
+  },
+});
+
 const Navbar = () => {
   const classes = useStyles();
   const [isSignedIn, setIsSignedIn] = useState(false);
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            BookFind
-          </Typography>
-          <Button color="inherit"><Link to="/">Home</Link></Button>
-          <Button color="inherit"><Link to="/books">Books</Link></Button>
-          <Button color="inherit"><Link to="/vendors">Vendors</Link></Button>
-          <Button color="inherit"><Link to="/about">About</Link></Button>
-          <Button color="inherit">{isSignedIn ? <Link to="/profile">Profile</Link>: <Link to="/sign-in">Login</Link>}</Button>
-        </Toolbar>
-      </AppBar>
+    <div className={classes.root}> 
+      <ThemeProvider theme={sty}>
+        <AppBar position="static" style={{ boxShadow: 'none', border: 'none' }}>
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              BookFind
+            </Typography>
+            <Button color="inherit" >
+              <Link to="/" style={{ textDecoration: "none" }}>Home</Link>
+            </Button>
+            <Button color="inherit">
+              <Link to="/books" style={{ textDecoration: "none" }}>Books</Link>
+            </Button>
+            <Button color="inherit">
+              <Link to="/vendors" style={{ textDecoration: "none" }}>Vendors</Link>
+            </Button>
+            <Button color="inherit">
+              <Link to="/about" style={{ textDecoration: "none" }}>About</Link>
+            </Button>
+            <Button color="inherit">
+              {isSignedIn ? (
+                <Link to="/profile" style={{ textDecoration: "none" }}>Profile</Link>
+              ) : (
+                <Link to="/sign-in" style={{ textDecoration: "none" }}>Login</Link>
+              )}
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </ThemeProvider>
     </div>
   );
-}
+};
 
 export default Navbar;
+
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -3,19 +3,44 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 const AboutUsPage = () => {
+  const styles = {
+    container: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+      width: "80%",
+      margin: "auto",
+    },
+    heading: {
+      fontSize: "36px",
+      fontWeight: "bold",
+      marginBottom: "20px",
+    },
+    paragraph: {
+      fontSize: "18px",
+      lineHeight: "1.5",
+      textAlign: "center",
+      maxWidth: "800px",
+    },
+  };
+
   return (
     <div>
       <Navbar />
-      <h1>Who We Are</h1>
-      <p>
-        Welcome to BookFind, the centralized website that transforms your book
-        search experience. With BookFind, you can effortlessly check book
-        availability across multiple stores, find the nearest options based on
-        your location, and enjoy a hassle-free search process. Discover new
-        reads and explore the world of literature with ease. Start your book
-        journey with BookFind today! Launched in July 2023.
-      </p>
-      <Footer/>
+      <div style={styles.container}>
+        <h1 style={styles.heading}>Who We Are</h1>
+        <p style={styles.paragraph}>
+          Welcome to BookFind, a centralized website that transforms your book
+          search experience. With BookFind, you can effortlessly check book
+          availability across multiple stores, find the nearest options based on
+          your location, and enjoy a hassle-free search process. Discover new
+          reads and explore the world of literature with ease. Start your book
+          journey with BookFind today! Launched in July 2023.
+        </p>
+
+        <Footer />
+      </div>
     </div>
   );
 };
