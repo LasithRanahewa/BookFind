7a20de6d8f9c8860539ba87d50eac477657a8a4c
Lasith Ranahewa <lasithmrana@gmail.com>
2023-09-05 12:12:11 +0530

    fix bug - vendor signup

--- client/src/pages/vendor/VendorSignUp.jsx
+++ client/src/pages/vendor/VendorSignUp.jsx
@@ -27,7 +27,7 @@ const Form = styled("form")({
 
 const TextFieldWrapper = styled(TextField)({
   margin: "8px",
-  width: "25ch",
+  width: "60%",
 });
 
 const ButtonWrapper = styled(Button)({
@@ -54,37 +54,12 @@ const VendorSignUp = () => {
     setProfilePicture(event.target.files[0]);
   };
 
-  const Form = styled("form")({
-    // margin: "3rem",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    // marginTop: "32px",
-    backgroundColor: "#DAE1E7",
-    minHeight: "100vh",
-  });
-
-  const TextFieldWrapper = styled(TextField)({
-    margin: "8px",
-    width: "60%",
-
-    "& .MuiOutlinedInput-root": {
-      "& fieldset": {
-        borderColor: "#27496D",
-        borderWidth: "0.1rem",
-      },
-      "&:hover fieldset": {
-        borderColor: "#27496D",
-      },
-      "&.Mui-focused fieldset": {
-        borderColor: "#27496D",
-      },
-    },
-    "& .MuiFormLabel-root": {
-      color: "#27496D",
-    },
-    "& .MuiInputBase-input": {
-      color: "#27496D",
+  const ButtonWrapper = styled(Button)({
+    margin: "16px",
+    backgroundColor: "#27496D",
+    color: "#fff",
+    "&:hover": {
+      backgroundColor: "#193A5E",
     },
   });
 
@@ -93,7 +68,7 @@ const VendorSignUp = () => {
       <ThemeProvider theme={theme}>
         <Grid container>
           <Grid item xs={12} sm={4}></Grid>
-          <Grid item xs={12} sm={8}>
+          <Grid item xs={12} sm={8} sx={{ backgroundColor: "white" }}>
             <Form onSubmit={handleSubmit}>
               <Typography
                 variant="h3"
@@ -126,7 +101,7 @@ const VendorSignUp = () => {
               <TextFieldWrapper
                 label="Address"
                 value={address}
-                onChange={(event) => setPhoneNumber(event.target.value)}
+                onChange={(event) => setAddress(event.target.value)}
               />
               <TextFieldWrapper
                 label="BRN"
@@ -151,21 +126,21 @@ const VendorSignUp = () => {
                   width: "60%",
                   "& .MuiOutlinedInput-root": {
                     "& fieldset": {
-                      borderColor: "#27496D",
+                      // borderColor: "#27496D",
                       borderWidth: "0.1rem",
                     },
                     "&:hover fieldset": {
-                      borderColor: "#27496D",
+                      // borderColor: "#27496D",
                     },
                     "&.Mui-focused fieldset": {
-                      borderColor: "#27496D",
+                      // borderColor: "#27496D",
                     },
                   },
                   "& .MuiFormLabel-root": {
-                    color: "#27496D",
+                    // color: "#27496D",
                   },
                   "& .MuiInputBase-input": {
-                    color: "#27496D",
+                    // color: "#27496D",
                   },
                   "& .MuiInputBase-input[type=file]": {
                     position: "relative",
@@ -181,7 +156,7 @@ const VendorSignUp = () => {
                     zIndex: "1",
                   },
                   "& .MuiInputBase-root::before": {
-                    color : "#27496D",
+                    // color: "#27496D",
                     // content: '"Choose File"',
                     position: "absolute",
                     top: "50%",
@@ -199,7 +174,6 @@ const VendorSignUp = () => {
                   <Checkbox
                     checked={agreeTerms}
                     onChange={(event) => setAgreeTerms(event.target.checked)}
-                    //   color="primary"
                   />
                 }
                 label="I agree to the terms and conditions"
@@ -207,7 +181,6 @@ const VendorSignUp = () => {
 
               <ButtonWrapper
                 variant="contained"
-                //   color="primary"
                 type="submit"
                 disabled={!agreeTerms}
               >
