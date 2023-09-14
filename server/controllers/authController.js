8a118aef81afb0f49a77dd5262eded1f6dcad103
RusaraBW <rusara.wimalasena123@gmail.com>
2023-09-14 17:08:38 +0530

    fix conflicts

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -8,58 +8,17 @@ const registerUser = async (req, res) => {
 	try {
 		const existingUser = await User.findOne({ email: req.body.email });
 
-// 		if (existingUser) {
-// 			return res.status(403).json({ success: false, message: "User already exists" });
-// 		}
-
-// 		const newUser = new User(req.body);
-// 		newUser.setPassword(req.body.password);
-
-// 		const savedUser = await newUser.save();
-
-// 		return res.status(201).json({ success: true, user: savedUser });
-// 	} catch (err) {
-// 		return res.status(400).json({ success: false, err });
-// 	}
-// };
-
-const registerUser = async(req, res) => {
-	try {
-		const { name, email, password } = req.body;
-
-		if( !name || !email || !password) {
-			res.status(400);
-			throw new Error("Please fill all the fields");
-
+		if (existingUser) {
+			return res.status(403).json({ success: false, message: "User already exists" });
 		}
 
-		const userExists = await User.findOne({ email: email });
-		console.log("OK!");
+		const newUser = new User(req.body);
+		newUser.setPassword(req.body.password);
 
-		if(userExists) {
-			res.status(400);
-			throw new Error("User already exists");
-		}
+		const savedUser = await newUser.save();
 
-		const salt = await bcrypt.genSalt(10);
-		const hashedPassword = await bcrypt.hash(password, salt);
-
-		const user = await User.create({
-			name,
-			email,
-			hashedPassword
-		});
-
-		if(user) {
-			res.status(201).json({
-				_id: user._id,
-				name: user.name,
-				email: user.email,
-				password: user.password
-			});
-		}
-	} catch(err) {
-		console.log(req.body);
+		return res.status(201).json({ success: true, user: savedUser });
+	} catch (err) {
 		return res.status(400).json({ success: false, err });
 	}
 };
