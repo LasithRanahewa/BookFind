cceebb558296ae86b59dd9614842523667246eca
RusaraBW <rusara.wimalasena123@gmail.com>
2023-09-14 18:39:29 +0530

    fix bugs in register

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -6,10 +6,6 @@ const router = express.Router();
 const {
     registerUser,
     loginLocalUser,
-    loginGoogleUser,
-    redirectGoogleUser,
-    loginFacebookUser,
-    redirectFacebookUser,
     logoutUser
 } = require("../controllers/authController");
 
@@ -20,14 +16,6 @@ router.post("/register", (req, res) => registerUser(req, res));
 router.get("/login", (req, res) => res.render("/login"));
 
 router.post("/login", (req, res, next) => loginLocalUser(req, res, next));
-
-// router.get("/auth/google", (req, res) => loginGoogleUser(req, res));
-
-// router.get("/auth/google/bookfind", (req, res) => redirectGoogleUser(req, res));
-
-// router.get("/auth/facebook", (req, res) => loginFacebookUser(req, res));
-
-// router.get("/auth/facebook/bookfind", (req, res) => redirectFacebookUser(req, res));
 
 router.get("/logout", (req, res, next) => logoutUser(req, res, next));
 
