63ce32f400434cd3b1bf9ff85def0013824887a5
unknown <2021cs207@stu.ucsc.cmb.ac.lk>
2023-09-15 06:57:34 +0530

    Trending card size- auto

--- client/src/components/bookfind-components/TrendingBookCard.jsx
+++ client/src/components/bookfind-components/TrendingBookCard.jsx
@@ -27,7 +27,7 @@ const TrendingBookCard = ({ book }) => {
     <Card style={{height: '100%'}}>
       <CardMedia
         component="img"
-        height="400"
+        height="auto"
         image={book.image}
         alt="Book cover"
       />
